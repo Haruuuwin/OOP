61dd0261bbeb535cde496f65d51e859f5baa7c83
Gia <gianellegarcia23@gmail.com>
2024-12-12 10:06:11 +0800

    Create LA2.py

---
+++ LA2.py
@@ -0,0 +1,12 @@
+class mobaylhero():
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+fighter = mobaylhero("Balmond", "fighter")
+assassin = mobaylhero("Saber", "assassin")
+
+print(fighter.name)
+print(fighter.role)
+print(assassin.name)
+print(assassin.role)
