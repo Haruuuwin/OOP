2d5ce1acc11944276f71aebcd52399ebd0d7c48a
Gia <gianellegarcia23@gmail.com>
2024-12-21 15:10:37 +0800

    OE's

---
+++ Other Evidences/OE5/OE_5.py
@@ -0,0 +1,298 @@
+Name: Gianelle S. Garcia
+Yr/Section: BSCS - 2A
+OE # 5 
+---------------------------------------------
+Game Title: Honkai Star Rail
+---------------------------------------------
+Game object 1: Himeko
+attributes:
+- Name: Himeko
+- HP
+- Role: Follow-up
+- Attack damage
+- rarity: 5 star
+- level
+- combat path: Erudition
+- combat type: Fire
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Victory Rush
+- lightcone: Before Dawn
+- basic attack: Sawblade Tuning
+- skill: Molten Detonation
+- ultimate: Heavenly Flare
+---------------------------------------------
+Game object 2: Kafka
+attributes:
+- Name: Kafka
+- HP
+- Role: Damage over Time
+- Attack damage
+- rarity: 5 star
+- level
+- combat path: Nihility
+- combat type: Lightning
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Gentle but Cruel
+- lightcone: Patience Is All You Need
+- basic attack: Midnight Tumult
+- skill: Caressing Moonlight
+- ultimate: Twilight Trill
+---------------------------------------------
+Game object 3: Welt
+attributes:
+- Name: Welt
+- HP
+- Role: sub DPS
+- Attack damage
+- rarity
+- level
+- combat path: Nihility
+- combat type: Imaginary
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Time Distortion
+- lightcone: In the Name of the World
+- basic attack: Gravity Suppression
+- skill: Edge of the Void
+- ultimate: Synthetic Black Hole
+---------------------------------------------
+Game object 4: Gepard
+attributes:
+- Name: Gepard
+- HP
+- Role: Support Defense
+- Attack damage
+- rarity: 5 star
+- level
+- combat path: Preservation
+- combat type: Ice
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Unyielding Will
+- lightcone: Moment of Victory
+- basic attack: Fist of Conviction
+- skill: Daunting Smite
+- ultimate: Enduring Bulwark
+---------------------------------------------
+Game object 5: Natasha
+attributes:
+- Name: Natasha
+- HP
+- Role: Healer
+- Attack damage
+- rarity: 4 star
+- level
+- combat path: Abundance
+- combat type: Physical
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Innervation
+- lightcone: Time Waits For No One
+- basic attack: Behind the Kindness
+- skill: Love, Heal, and Choose
+- ultimate: Gift of Rebirth
+---------------------------------------------
+Game object 6: Firefly
+attributes:
+- Name: Firefly
+- HP
+- Role: DPS
+- Attack damage
+- rarity: 5 star
+- level
+- combat path: Destruction
+- combat type: Fire
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Chrysalid Pyronexus
+- lightcone: Whereabouts Should Dreams Rest
+- basic attack: Order: Flare Propulsion
+- skill: Order: Aerial Bombardment
+- ultimate: Fyrefly Type-IV: Complete Combustion
+---------------------------------------------
+Game object 7: Acheron
+attributes:
+- Name: Acheron
+- HP
+- Role: DPS
+- Attack damage
+- rarity: 5 star
+- level
+- combat path: Nihility
+- combat type: lightning
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Atop Rainleaf Hangs Oneness
+- lightcone: Along the Passing Shore
+- basic attack: Trilateral Wiltcross
+- skill: Order: Octobolt Flash
+- ultimate: Slashed Dream Cries in Red
+---------------------------------------------
+Game object 8: Black Swan
+attributes:
+- Name: Black Swan
+- HP
+- Role: DoT
+- Attack damage
+- rarity: 5 star
+- level
+- combat path: Nihility
+- combat type: wind
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Loom of Fate's Caprice
+- lightcone: Reforged Remembrance
+- basic attack: Percipience, Silent Dawn
+- skill: Decadence, False Twilight
+- ultimate: Bliss of Otherworld's Embrace
+---------------------------------------------
+Game object 9: Qingque
+attributes:
+- Name: Qingque
+- HP
+- Role: DPS
+- Attack damage
+- rarity: 4 star
+- level
+- combat path: Erudition
+- combat type: Quantum
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Celestial Jade
+- lightcone: Before Dawn
+- basic attack: Flower Pick
+- skill: A Scoop of Moon
+- ultimate: A Quartet? Woo-hoo!
+---------------------------------------------
+Game object 10: Lynx
+attributes:
+- Name: Lynx
+- HP
+- Role: Healer
+- Attack damage
+- rarity: 4 star
+- level
+- combat path: Abundance
+- combat type: Quantum
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Outdoor Survival Experience
+- lightcone: Time Waits For No One
+- basic attack: Ice Crampon Technique
+- skill: Salted Camping Cans
+- ultimate: Snowfield First Aid
+---------------------------------------------
+Game object 11: Dan Heng
+attributes:
+- Name: Dan Heng
+- HP
+- Role: DoT
+- Attack damage
+- rarity: 4 star
+- level
+- combat path: The Hunt
+- combat type: Wind
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Outdoor Survival Experience
+- lightcone: In the Night
+- basic attack: Cloudlancer Art: North Wind
+- skill: Cloudlancer Art: Torrent
+- ultimate: Superiority of Reach
+---------------------------------------------
+Game object 12: Gallagher
+attributes:
+- Name: Gallagher
+- HP
+- Role: Healer
+- Attack damage
+- rarity: 4 star
+- level
+- combat path: Abundance
+- combat type: Fire
+- break effect
+- speed
+- defense
+- crit dmg
+- crit rate
+
+methods:
+- talent: Tipsy Tussle
+- lightcone: Time Waits For No One
+- basic attack: Nectar Blitz
+- skill: Special Brew
+- ultimate: Champagne Etiquette
+
+---------------------------------------------
+Game object 13: Ruan Mei
+Game object 14: Stelle
+Game object 15: Aventurine
+Game object 16: Herta
+Game object 17: March 7th
+Game object 18: Silverwolf
+Game object 19: Blade
+Game object 20: Yanqing
+Game object 21: Yunli
+Game object 22: Argenti
+Game object 23: Arlan
+Game object 24: Asta
+Game object 25: Bailu
+Game object 26: Dr. Ratio
+Game object 27: Clara
